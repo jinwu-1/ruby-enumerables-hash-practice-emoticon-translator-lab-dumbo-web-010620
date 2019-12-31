0e50301a11846b06a39aa78c7f990876b9074385
Jin Wu <jinn.wuu@gmail.com>
2019-12-31 15:39:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,13 @@
 # require modules here
 require 'yaml'
 
-def load_library
-  emoticon = YAML.load_file('./lib/emoticons.yml')
+def load_library(file_path)
+  library = YAML.load_file(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticons|
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
+
 end
 
 def get_japanese_emoticon
